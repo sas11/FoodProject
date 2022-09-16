7372c7638e589c3c7029594445ce5d36451c6596
Aliaksandr <sanya2798@gmail.com>
2022-09-16 18:16:37 +0300

    UP project 'server-json'

--- js/script.js
+++ js/script.js
@@ -315,4 +315,9 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000);
     }
+
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res));
+
 });
