211865be8b73b69d0651af8368b635aa822b5506
Aliaksandr <sanya2798@gmail.com>
2022-09-16 19:11:15 +0300

    UP project 'alt variant card menu'

--- js/script.js
+++ js/script.js
@@ -182,7 +182,7 @@ window.addEventListener('DOMContentLoaded', () => {
                              <div class="menu__item-price">
                                  <div class="menu__item-cost">Цена:</div>
                                 <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                         </div>\
+                         </div>
             
             `;
             this.parentSelector.append(element);
@@ -207,6 +207,31 @@ window.addEventListener('DOMContentLoaded', () => {
                 new MenuCard(img, altimg,title, descr, price, '.menu .container').render();
             });
         });
+
+
+    //Alternative variant
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => createCard(data));
+    
+    // function createCard(data) {
+    //     data.forEach(({img,altimg,title,descr,price}) => {
+    //         const element = document.createElement('div');
+
+    //         element.classList.add('menu__item');
+
+    //         element.innerHTML = `
+    //         <img src=${img} alt=${altimg}>
+    //         <h3 class="menu__item-subtitle">${title}</h3>
+    //         <div class="menu__item-descr">${descr}</div>
+    //         <div class="menu__item-divider"></div>
+    //         <div class="menu__item-price">
+    //             <div class="menu__item-cost">Цена:</div>
+    //            <div class="menu__item-total"><span>${price*27}</span> грн/день</div>
+    //     </div>
+    //         `;
+    //         document.querySelector('.menu .container').append(element);
+    //     });
+    // }
 
     //Forms
 
